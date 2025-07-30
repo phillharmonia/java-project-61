f91c162c310ee66cc90ef049469938b353c97bfb
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-07-30 18:44:52 +0700

    create class Calc

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,12 @@
+package hexlet.code.games;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Calc {
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+    }
+}
