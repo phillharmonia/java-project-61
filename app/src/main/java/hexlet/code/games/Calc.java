94fb3bc9b121d11a41e77b3953f238920c8f1468
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-07-30 20:15:58 +0700

    create calc game

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,12 +1,37 @@
 package hexlet.code.games;
 
-import java.util.Random;
+import hexlet.code.utills.ExpressionGenerator;
+import hexlet.code.utills.Greeting;
+import hexlet.code.utills.Messages;
+
 import java.util.Scanner;
 
 public class Calc {
     public static void game() {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
+        var userName = Greeting.greet();
+        System.out.println("What is the result of the expression?");
+
+        int correctAnswers = 0;
+        while (correctAnswers < 3) {
+            int a = ExpressionGenerator.generateNumber(20);
+            int b = ExpressionGenerator.generateNumber(20);
+            char operator = ExpressionGenerator.generateOperator();
+            var question = a + " " + operator + " " + b + " ";
+
+            Messages.printQuestion(question);
+            Messages.printYourAnswer();
+
+            int answer = scanner.nextInt();
+            int correctAnswer = ExpressionGenerator.calculate(a, b, operator);
+            if (answer != correctAnswer) {
+                Messages.printWrongAnswer(answer, correctAnswer, userName);
+                return;
+            }
+            Messages.printCorrect();
+            correctAnswers+=1;
+        }
+        Messages.printCongratulations(userName);
+        scanner.close();
     }
 }
