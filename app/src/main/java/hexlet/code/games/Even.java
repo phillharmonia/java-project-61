83106c15fcc941408f02838d983306581ef13e57
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-07-30 19:21:49 +0700

    refactoring

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -9,23 +9,24 @@ public class Even {
     public static void game() {
         Scanner scanner = new Scanner(System.in);
         var userName = Greeting.greet();
-        Greeting.greet();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-        int correctAnswers= 0;
+        int correctAnswers = 0;
         while (correctAnswers < 3) {
             int number = ExpressionGenerator.generateNumber(100);
-            System.out.println("Question: " + number);
-            System.out.print("Your answer: ");
+            var question = number + "";
+            Messages.printQuestion(question);
+            Messages.printYourAnswer();
+
             String answer = scanner.nextLine();
             String correctAnswer = number % 2 == 0 ? "yes" : "no";
             if(!answer.equals(correctAnswer)) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, " + userName + "!");
+                Messages.printWrongAnswer(answer, correctAnswer, userName);
                 return;
             }
-            System.out.println("Correct!");
+            Messages.printCorrect();
             correctAnswers += 1;
         }
-        System.out.println("Congratulations, " + userName + "!");
+        Messages.printCongratulations(userName);
     }
 }
