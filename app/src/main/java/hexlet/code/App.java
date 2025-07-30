1dff060f22b31d532d060a58ee614dc2c723e277
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-07-30 12:56:39 +0700

    create menu for choice game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Enter your choice: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Cli.greet();
+                break;
+            case 2:
+                Even.game();
+                break;
+            case 0:
+                System.out.println("Bye!");
+            default:
+                System.out.println("Invalid choice.");
+        }
+        scanner.close();
     }
 }
