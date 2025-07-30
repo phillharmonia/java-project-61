912a795aace68a4c2582135143c37a0f5656ade6
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-07-30 18:45:33 +0700

    create utill class Greeting

---
+++ app/src/main/java/hexlet/code/utills/Greeting.java
@@ -0,0 +1,14 @@
+package hexlet.code.utills;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static String greet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        return userName;
+    }
+}
