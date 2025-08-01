0e2e1d564e7eed1f34d92202311bde8819db1376
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-08-01 18:37:59 +0700

    create isPrime expression

--- app/src/main/java/hexlet/code/utills/ExpressionGenerator.java
+++ app/src/main/java/hexlet/code/utills/ExpressionGenerator.java
@@ -35,4 +35,21 @@ public class ExpressionGenerator {
         }
         return progression;
     }
+    public static String isPrime(int number) {
+        if (number < 2) {
+            return "false";
+        }
+        if (number == 2) {
+            return "true";
+        }
+        if (number % 2 == 0) {
+            return "false";
+        }
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return "false";
+            }
+        }
+        return "true";
+    }
 }
