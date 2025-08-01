91ab4864eee0a4b6a1262eedf966bab1135228ff
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-08-01 14:55:46 +0700

    create Gcd game

---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,37 @@
+package hexlet.code.games;
+
+import hexlet.code.utills.ExpressionGenerator;
+import hexlet.code.utills.Greeting;
+import hexlet.code.utills.Messages;
+
+import java.util.Scanner;
+
+public class Gcd {
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+        var userName = Greeting.greet();
+        System.out.println("Find the greatest common divisor of given numbers. ");
+
+        int correctAnswers = 0;
+        while(correctAnswers < 3) {
+            int nod = ExpressionGenerator.generateNumber(1, 25);
+            int a = ExpressionGenerator.generateNumber(1, 10) * nod;
+            int b = ExpressionGenerator.generateNumber(1, 10) * nod;
+            var question = a + " " + b + " ";
+            Messages.printQuestion(question);
+            Messages.printYourAnswer();
+            
+            int answer = scanner.nextInt();
+            int correctAnswer = ExpressionGenerator.gcd(a, b);
+
+            if (answer != correctAnswer) {
+                Messages.printWrongAnswer(answer, correctAnswer, userName);
+                return;
+            }
+            Messages.printCorrect();
+            correctAnswers+=1;
+        }
+        Messages.printCongratulations(userName);
+        scanner.close();
+    }
+}
