a9218af729f5d9e3011c90fa94d1b2b77c890914
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-08-01 17:35:32 +0700

    create Progression game

---
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,47 @@
+package hexlet.code.games;
+
+import hexlet.code.utills.ExpressionGenerator;
+import hexlet.code.utills.Greeting;
+import hexlet.code.utills.Messages;
+
+import java.util.Scanner;
+
+public class Progression {
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+        var userName = Greeting.greet();
+        System.out.println("What number is missing in the progression?");
+
+        int correctAnswers = 0;
+        while (correctAnswers < 3) {
+            int start = ExpressionGenerator.generateNumber(1, 50);
+            int step = ExpressionGenerator.generateNumber(1, 10);
+            int length = 10;
+            int[] progression = ExpressionGenerator.generateProgression(start, step, length);
+            int hiddenIndex = ExpressionGenerator.generateNumber(1, length);
+            int correctValue = progression[hiddenIndex];
+
+            StringBuilder question = new StringBuilder();
+            for (int i = 0; i < progression.length; i++) {
+                if (i == hiddenIndex) {
+                    question.append(".. ");
+                } else {
+                    question.append(progression[i]).append(" ");
+                }
+            }
+            Messages.printQuestion(question.toString());
+            Messages.printYourAnswer();
+
+            int answer = scanner.nextInt();
+
+            if (answer != correctValue) {
+                Messages.printWrongAnswer(answer, correctValue, userName);
+                return;
+            }
+            Messages.printCorrect();
+            correctAnswers+=1;
+        }
+        Messages.printCongratulations(userName);
+        scanner.close();
+    }
+}
