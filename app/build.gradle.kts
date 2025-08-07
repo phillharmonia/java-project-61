9d04b93bb81f2629d37b2dd92f143da97b920a4f
Konstantin Gavrilov <tiltozavrik@yandex.ru>
2025-08-07 16:16:41 +0700

    delete tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,15 +11,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 tasks.withType<JavaCompile> {
     options.compilerArgs.add("--enable-preview")
 }
